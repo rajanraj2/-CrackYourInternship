1a8824d88a3003e2f5de8fe61e961738474063dc
Ruggy Mango <rans2203@gmail.com>
2024-07-17 15:58:09 +0530

    Add string problem 1

---
+++ String/01_Valid_Parantheses.cpp
@@ -0,0 +1,25 @@
+// Link - https://leetcode.com/problems/valid-parentheses/
+// Valid Parantheses
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                if (st.empty() || 
+                (ch == ')' && st.top() != '(') || 
+                (ch == '}' && st.top() != '{') || 
+                (ch == ']' && st.top() != '[')) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
