2002cf25f213a43014f2815f303ccdc03a74dc28
Ruggy Mango <rans2203@gmail.com>
2024-07-18 22:45:31 +0530

    Add array problem 20

---
+++ Array/20_Jump_Game.cpp
@@ -0,0 +1,17 @@
+// Link - https://leetcode.com/problems/jump-game/
+// Jump Game
+
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i > maxReach) return false;
+            maxReach = max(maxReach, i + nums[i]);
+            if (maxReach >= n - 1) return true;
+        }
+        return false;
+    }
+};
