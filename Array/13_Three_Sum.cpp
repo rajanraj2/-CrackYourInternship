4695af30b2e00e04eaae91454bbc402b532d82e2
Ruggy Mango <rans2203@gmail.com>
2024-07-18 15:58:39 +0530

    Add array problem 13

---
+++ Array/13_Three_Sum.cpp
@@ -0,0 +1,34 @@
+// Link - https://leetcode.com/problems/3sum/
+// Three Sum Problem
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) return {};
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) j++;
+                else if (sum > 0) k--;
+                else {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+        return result;
+    }
+};
