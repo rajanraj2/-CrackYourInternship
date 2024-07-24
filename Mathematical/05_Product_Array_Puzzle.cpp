fc8cd1e8b075908849ff8494712832057d902e70
Ruggy Mango <rans2203@gmail.com>
2024-07-24 20:40:05 +0530

    Add mathematical problem 5

---
+++ Mathematical/05_Product_Array_Puzzle.cpp
@@ -0,0 +1,26 @@
+// Link - https://www.geeksforgeeks.org/problems/product-array-puzzle4525/1
+// Product array puzzle
+
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+        
+        vector<long long int> left(n, 1);
+        vector<long long int> right(n, 1);
+        vector<long long int> result(n, 1);
+        
+        for (int i = 1; i < n; ++i) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        for (int i = 0; i < n; i++) {
+            result[i] = left[i] * right[i];
+        }
+        return result;
+    }
+};
