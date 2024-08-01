8fec56472bad8714ebea05575a55337fe3567e2a
Ruggy Mango <rans2203@gmail.com>
2024-08-01 23:53:28 +0530

    Add linked list problem 13

---
+++ LinkedList/13_Add_Two_Numbers.cpp
@@ -0,0 +1,45 @@
+// Link - https://leetcode.com/problems/add-two-numbers/
+// Add two numbers
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* l3 = dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr) {
+            int sum = carry;
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            l3->next = new ListNode(sum % 10);
+            l3 = l3->next;
+        }
+
+        if (carry > 0) {
+            l3->next = new ListNode(carry);
+        }
+
+        return dummy->next;
+    }
+};
